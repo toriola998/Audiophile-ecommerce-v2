54fb7712d0a9f044130e27bdfe0cf401179a0b78
Toriola <faaedah@gmail.com>
2023-05-14 22:02:01 +0100

    ADD: added tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
 
+export default defineNuxtConfig({
+    css: ['@/assets/css/main.css'],
+    modules: [
+        '@nuxtjs/tailwindcss'
+    ]
 })
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="pt-40 container mx-auto bg-gray-300 p-8">
+        <h1>Hello Worrldd</h1>
+    </div>
+</template>
+
+<style scoped>
+@import '@/assets/css/main.css';
+</style>
