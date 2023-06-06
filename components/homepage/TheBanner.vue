703ecf971f12c36db3148a334b4d74fcc75cdfb0
Toriola <faaedah@gmail.com>
2023-06-06 17:54:10 +0100

    ADD: added tablet & desktop view to banner

--- components/homepage/TheBanner.vue
+++ components/homepage/TheBanner.vue
@@ -1,23 +1,27 @@
 <template>
-    <header class="px-5 bg-black">
-        <div>
+    <header class="px-5">
+        <div class="container">
+        <div class="max-w-[379px] mx-auto lg:mx-[unset]">
             <p class="opacity-50 tracking-[0.62rem]">NEW PRODUCT</p>
-            <h1 class="font-bold uppercase text-[2.3rem] leading-10 mt-4 mb-6">XX99 Mark II<br> HeadphoneS</h1>
+            <h1 class="font-bold uppercase text-[2.3rem] md:text-[3.35rem] leading-10 md:leading-[3.5rem] mt-4 mb-6">XX99 Mark II<br> HeadphoneS</h1>
             <p class="opacity-75 mb-7">Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
-            <div class="flex justify-center">
+            <div class="flex justify-center lg:justify-start">
                 <SharedButtonLink cta-link="/" cta-text="SEE PRODUCT" class="orange btn-link"/>
             </div>
         </div>
+    </div>
     </header>
 </template>
 
 <style scoped lang="scss">
   header {
-    @apply h-[500px] bg-[url('/images/home/mobile/image-header.jpg')] 
-    bg-cover bg-center bg-no-repeat flex items-center;
+    @apply h-[500px] bg-cover bg-center bg-no-repeat flex items-center bg-black
+    bg-[url('/images/home/mobile/image-header.jpg')] 
+    md:h-[600px] md:bg-[url('/images/home/tablet/image-header.jpg')] 
+    lg:bg-[url('/images/home/desktop/image-hero.jpg')];
 
     h1, p { 
-      @apply text-center text-light;
+      @apply text-center text-light lg:text-start;
     }
 
     p {
