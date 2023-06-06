c3471b5baa5444b827089c874b3f08ec17cb7819
Toriola <faaedah@gmail.com>
2023-06-06 16:18:40 +0100

    REFACTOR: added formatting

--- components/homepage/TheBanner.vue
+++ components/homepage/TheBanner.vue
@@ -1,14 +1,14 @@
 <template>
-  <header class="px-5 bg-black">
-    <div>
-      <p class="opacity-50 tracking-[0.62rem]">NEW PRODUCT</p>
-      <h1 class="font-bold uppercase text-[2.3rem] leading-10 mt-4 mb-6">XX99 Mark II<br> HeadphoneS</h1>
-      <p class="opacity-75 mb-7">Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
-      <div class="flex justify-center">
-        <SharedButtonLink cta-link="/" cta-text="SEE PRODUCT" class="orange btn-link"/>
-      </div>
-    </div>
-  </header>
+    <header class="px-5 bg-black">
+        <div>
+            <p class="opacity-50 tracking-[0.62rem]">NEW PRODUCT</p>
+            <h1 class="font-bold uppercase text-[2.3rem] leading-10 mt-4 mb-6">XX99 Mark II<br> HeadphoneS</h1>
+            <p class="opacity-75 mb-7">Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
+            <div class="flex justify-center">
+                <SharedButtonLink cta-link="/" cta-text="SEE PRODUCT" class="orange btn-link"/>
+            </div>
+        </div>
+    </header>
 </template>
 
 <style scoped lang="scss">
