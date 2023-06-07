ec56fef9b080576f1c0e49e79121dc87e9e4af97
Toriola <faaedah@gmail.com>
2023-06-07 17:45:34 +0100

    ADD: added product layout

---
+++ components/product/TheProduct.vue
@@ -0,0 +1,22 @@
+<template>
+    <section class="mb-[120px] each-product">
+        <img src="" alt="" />
+        <div class="">
+            <p class="tracking-[0.62rem] text-deep-orange text-sm">NEW PRODUCT</p>
+            <h2 class="font-bold text-[1.75rem] md:text-[2.2rem] my-6">YX1 WIRELESS EARPHONES</h2>
+            <p class="opacity-50 text-[15px]">Tailor your listening experience with bespoke dynamic drivers from the new YX1 Wireless Earphones. 
+            Enjoy incredible high-fidelity sound even in noisy environments with its active noise cancellation feature.</p>
+            <div class="flex justify-center lg:justify-start mt-6">
+                <SharedButtonLink cta-link="/" cta-text="SEE PRODUCT" class="orange btn-link"/>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style scoped lang="scss">
+.each-product {
+    h2, p {
+        @apply text-center lg:text-start; 
+    }
+}
+</style>
--- pages/product/slug.vue
+++ pages/product/slug.vue
@@ -1,7 +1,9 @@
 <template>
     <ProductTheHeader />
-    <main>
-       
+    <main class="bg-grey-2">
+        <div class="px-5 pt-10 lg:pt-24">
+            <ProductTheProduct class="container"/>
+        </div>
         <SharedProductCategory />
         <SharedAboutUs />
     </main>
