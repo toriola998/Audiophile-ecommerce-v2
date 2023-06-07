d27f3256b4de14b0d2fb33b562080e1d6b87ebb7
Toriola <faaedah@gmail.com>
2023-06-07 18:04:14 +0100

    ADD: added all respective props

--- components/product/TheProduct.vue
+++ components/product/TheProduct.vue
@@ -1,11 +1,18 @@
 <template>
     <section class="mb-[120px] each-product">
-        <img src="" alt="" />
+        <picture>
+            <source media="(min-width: 1000px)" 
+                    :srcset="`image/${product.image.desktop}`">
+            <source media="(min-width: 600px)" 
+                    :srcset="`image/${product.image.tablet}`">
+            <img :src="`image/${product.image.mobile}`" 
+                    :alt="product.name"
+                    class="rounded-lg">
+        </picture>
         <div class="">
-            <p class="tracking-[0.62rem] text-deep-orange text-sm">NEW PRODUCT</p>
-            <h2 class="font-bold text-[1.75rem] md:text-[2.2rem] my-6">YX1 WIRELESS EARPHONES</h2>
-            <p class="opacity-50 text-[15px]">Tailor your listening experience with bespoke dynamic drivers from the new YX1 Wireless Earphones. 
-            Enjoy incredible high-fidelity sound even in noisy environments with its active noise cancellation feature.</p>
+            <p class="tracking-[0.62rem] text-deep-orange text-sm" v-if="product.new">NEW PRODUCT</p>
+            <h2 class="font-bold text-[1.75rem] md:text-[2.2rem] my-6">{{ product.name }}</h2>
+            <p class="opacity-50 text-[15px]">{{ product.description }}</p>
             <div class="flex justify-center lg:justify-start mt-6">
                 <SharedButtonLink cta-link="/" cta-text="SEE PRODUCT" class="orange btn-link"/>
             </div>
