e2fba4e4bb0b016dc816d6f16a04c4392a52a4ec
Toriola <faaedah@gmail.com>
2023-06-07 17:20:08 +0100

    ADD: added header to product page

---
+++ components/product/TheHeader.vue
@@ -0,0 +1,16 @@
+<template>
+    <header class="bg-black">
+        <h1 class="uppercase text-light text-[1.75rem] md:text-[2.7rem] text-center 
+        font-bold h-40 md:h-60 flex items-center justify-center">SPEAKERS
+        </h1>
+    </header>
+</template>
+
+<script setup lang="ts">
+defineProps({
+    title:{
+        type: String,
+        default: '',
+    },
+})
+</script>
---
+++ pages/product/slug.vue
@@ -0,0 +1,3 @@
+<template>
+    <ProductTheHeader />
+</template>
