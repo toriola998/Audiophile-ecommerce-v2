280377b68a7f13c916559c46da1de860b0b7dce4
Toriola <faaedah@gmail.com>
2023-06-10 13:04:10 +0100

    ADD: completed product details banner

--- components/product-details/TheBanner.vue
+++ components/product-details/TheBanner.vue
@@ -1,19 +1,23 @@
 <template>
-    <picture class="lg:w-[45%]">
-        <source media="(min-width: 1000px)" 
-                :srcset="`/images/${product.categoryImage.desktop}`">
-        <source media="(min-width: 500px)" 
-                :srcset="`/images/${product.categoryImage.tablet}`">
-        <img :src="`/images/${product.categoryImage.mobile}`" 
-                :alt="product.name"
-                class="rounded-lg">
-    </picture>
-    <div class="lg:w-[45%] xl:w-[40%]">
-        <p class="tracking-[0.62rem] text-deep-orange text-sm mt-10 lg:mt-0" v-if="product.new">NEW PRODUCT</p>
-        <h2 class="font-bold text-[1.75rem] md:text-[2.2rem] pt-6 uppercase lg:max-w-[300px]">{{ product.name }}</h2>
-        <p class="opacity-50 text-[15px] md:text-[1rem] leading-6 mp-6 lg:py-8">{{ product.description }}</p>
-        <div class="flex justify-center lg:justify-start mt-2">
-            <SharedButtonLink cta-link="/" cta-text="SEE PRODUCT" class="orange btn-link"/>
+    <div class="px-5 pb-10">
+        <div class="container md:flex items-center gap-20">
+            <picture class="md:w-[45%]">
+                <source media="(min-width: 1000px)" 
+                        :srcset="`/images/${product.image.desktop}`">
+                <source media="(min-width: 500px)" 
+                        :srcset="`/images/${product.image.tablet}`">
+                <img :src="`/images/${product.image.mobile}`" 
+                        :alt="product.name"
+                        class="rounded-lg">
+            </picture>
+            <div class="md:w-[45%] xl:w-[40%]">
+                <p class="tracking-[0.62rem] text-deep-orange text-sm mt-10 lg:mt-0" v-if="product.new">NEW PRODUCT</p>
+                <h2 class="font-bold text-[1.75rem] lg:text-[2.5rem] pt-6 uppercase lg:max-w-[300px] leading-[2.5rem]">{{ product.name }}</h2>
+                <p class="opacity-50 text-[15px] md:text-[1rem] leading-6 mt-6 lg:mb-10">{{ product.description }}</p>
+                <p class="font-bold text-[1.12rem] my-8">$ {{ product.price }}</p>
+                
+                <SharedAddToCart />
+            </div>
         </div>
     </div>
 </template>
---
+++ components/shared/AddToCart.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="flex gap-2">
+        <div class="flex justify-between items-center bg-grey-1 px-4 w-28">
+            <button class="text-sm opacity-25">-</button>
+            <p>2</p>
+            <button class="text-sm opacity-25">+</button>
+        </div>
+        <button class="orange btn-link">ADD TO CART</button>
+    </div>
+</template>
