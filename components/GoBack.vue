47616172051a80763da00a9ffaf451e2eba21157
Toriola <faaedah@gmail.com>
2023-06-11 09:00:34 +0100

    ADD: made goback button a component

---
+++ components/GoBack.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="container">
+        <button 
+            class="opacity-50 my-4 text-[15px] px-5 lg:my-10 lg:px-0 hover:text-deep-orange hover:opacity-[unset]" 
+            @click="$router.back()"
+            > Go back
+        </button>
+    </div>
+</template>
