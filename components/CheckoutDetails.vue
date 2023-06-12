60648cb298eed2798a09240c62c6700c83e59d13
Toriola <faaedah@gmail.com>
2023-06-12 11:38:31 +0100

    ADD: completed checkout form implementation

---
+++ components/CheckoutDetails.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="mb-8 checkout">
+        <p class="title">{{ title }}</p>
+        <div class="mb-4" v-for="(item, index) in details" :key="index">
+            <label for="">{{ item.label }}</label><br>
+            <input id="" :type="item.type" :placeholder="item.placeholder" class="input-wrap"/>
+        </div>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    title:{
+        type: String,
+        default: '',
+    },
+    details:{
+        type: Array,
+        default: () => [],
+    },
+})
+</script>
--- pages/checkout.vue
+++ pages/checkout.vue
@@ -1,24 +1,27 @@
 <template>
-    <main class="bg-grey-2 pb-20 checkout">
+    <main class="bg-grey-2 pb-20">
         <GoBack />
         <div class="px-5">
             <div class="container">
                 <section class="rounded-lg bg-light py-5 lg:py-10 px-3">
                     <h1 class="text-[1.5rem] md:text-[2rem] pb-7 font-bold">CHECKOUT</h1>
+                    <CheckoutDetails title="BILLING DETAILS" :details="billingDetails" />
+                    <CheckoutDetails title="SHIPPING INFO" :details="shippingInfo" />
                     
                     <div class="mb-8">
-                        <p class="title">BILLING DETAILS</p>
-                        <div class="mb-4" v-for="(item, index) in billingDetails" :key="index">
-                            <label for="">{{ item.label }}</label><br>
-                            <input id="" :type="item.type" :placeholder="item.placeholder" />
+                        <p class="title">PAYMENT DETAILS</p>
+                        <div class="mb-4 items-center flex gap-2 input-wrap " v-for="(item, index) in paymentDetails" :key="index">
+                            <input id="" type="radio" class="radio-input"/>
+                            <label for="" class="font-bold text-[0.75rem]">{{ item.label }}</label><br>
                         </div>
-                    </div>
 
-                    <div class="mb-8">
-                        <p class="title">SHIPPING INFO</p>
-                        <div class="mb-4" v-for="(item, index) in shippingInfo" :key="index">
-                            <label for="">{{ item.label }}</label><br>
-                            <input id="" :type="item.type" :placeholder="item.placeholder" />
+                        <CheckoutDetails title="" :details="paymentMethods" class="mt-8" />
+
+                        <div class="flex flex-col gap-3 mt-8">
+                            <img src="/images/Shape.png" alt="" role="presentation" height="48" width="48" />
+                            <p class="opacity-50 text-[.95rem]">The 'Cash on Delivery' option enables you to pay in cash when our delivery 
+                            courier arrives at your residence. Just make sure your address is correct so 
+                            that your order will not be cancelled.</p>
                         </div>
                     </div>
                 </section>
@@ -78,31 +81,44 @@ const shippingInfo = ref([
         value: '',
     }
 ]).value
+
+const paymentDetails = ref([
+    {
+        label: 'Payment Method',
+        value: '',
+    },
+    {
+        label: 'Cash on Delivery',
+        value: '',
+    },
+]).value
+
+const paymentMethods = ref([
+    {
+        label: 'e-Money Number',
+        placeholder: '238521993',
+        type: 'number',
+        value: '',
+    },
+    {
+        label: 'e-Money PIN',
+        placeholder: '6891',
+        type: 'number',
+        value: '',
+    },
+]).value
 </script>
 
 <style scoped lang="scss">
-.checkout {
-   .title {
-        @apply text-deep-orange font-bold text-[0.82rem] mb-4;
-   }
-}
-
-label {
-    @apply font-bold text-[0.75rem];
-}
-
-input {
-    @apply pl-6 py-3 rounded-md mt-2 w-full;
-    border: 1px solid #CFCFCF;
-
-        &:focus,
-        &:hover {
-            outline: 1px solid #D87D4A;
-        }
-
-        &::placeholder {
-            @apply text-sm opacity-40 font-bold;
-            color: black;
+div.input-wrap {
+    
+    &:hover {
+        border: 1px solid #D87D4A;
+        cursor: pointer;
     }
+}
+
+.radio-input {
+    accent-color: #D87D4A;
 }
 </style>
