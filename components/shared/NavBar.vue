60ae0d0bc9704782ea84d80fc78a7b327a051238
Toriola <faaedah@gmail.com>
2023-06-15 12:53:58 +0100

    added nav-bar logic

--- components/shared/NavBar.vue
+++ components/shared/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="px-5 h-[90px] z-40 w-full" :class="bgColor">
         <div class="flex justify-between h-[inherit] items-center border-b border-grey-4 container">
-            <button class="md:hidden">
+            <button class="md:hidden" @click="showNav = true">
                 <img src="/images/shared/tablet/icon-hamburger.svg" alt="" height="16" width="16"/>
             </button>
 
@@ -29,16 +29,19 @@
         </div>
         
         <!--MOBILE NAV LINKS--->
-        <div class="md:hidden">
-            <div class="overlay" />
+        <div class="md:hidden" v-if="showNav === true">
+            <div class="overlay" @click="showNav = false" />
             <SharedProductCategory class="bg-light absolute w-full right-0 left-0 rounded-b-lg z-50"/>
         </div>
     </nav>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import { useNavLinks } from '~/composables/useNavLinks';
 const { links } = useNavLinks();
+
+let showNav = ref(null);
 
 defineProps({
     bgColor:{
